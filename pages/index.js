51ee30d9167c05e0696fe4bc69f6b34f9447467e
Seulgi <hanny1226@naver.com>
2022-03-31 00:05:35 +0900

    init commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React
+import AppLayout from "../components/AppLayout"; form 'react';
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>Hello, Next!</div>
+    </AppLayout>
+  );
+}
+
+export default Home;
