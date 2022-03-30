5a511a563f50065aedff440be25d2f87cec11df1
Seulgi <hanny1226@naver.com>
2022-03-31 00:04:09 +0900

    Hello, Next js

---
+++ components/AppLayout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link'
+
+const AppLayout = ({children}) => {
+  return(
+    <div>
+      <div>
+      <Link href="/"><a>노드 버드</a></Link>
+      <Link href="/profile"><a>프로필</a></Link>
+      <Link href="/signup"><a>회원가입</a></Link>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+AppLayout.prototype = {
+  children: PropTypes.node.isRequired,
+};
+export default AppLayout;
