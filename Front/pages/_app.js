8f5d4ccfbb365e3f5b4848ba8050489bc8c40d62
Seulgi <hanny1226@naver.com>
2022-04-02 00:03:15 +0900

    initialization

---
+++ Front/pages/_app.js
@@ -0,0 +1,19 @@
+// import '../styles/globals.css'
+//
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+//
+// export default MyApp
+import React from 'react';
+import AppLayout from "../componets/AppLayout";
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>Hello, Next!</div>
+    </AppLayout>
+  );
+}
+
+export default Home;
