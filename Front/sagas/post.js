296873bcfbcc8cbd58b8caf8322ba7ae2b5d1332
Seulgi <hanny1226@naver.com>
2022-04-06 13:46:33 +0900

    Action and state summary

--- Front/sagas/post.js
+++ Front/sagas/post.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import {delay, put, takeLatest, all, fork} from "redux-saga/effects";
+import {ADD_COMMENT_FAILURE, ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_POST_FAILURE, ADD_POST_REQUEST, ADD_POST_SUCCESS} from "../reducers/post";
+
 
 function addPostAPI(data) { //얘는 *붙이면 error 발생
   return axios.post('/api/post', data);
@@ -10,24 +12,48 @@ function* addPost(action) {
     yield delay(1000);
     // const result = yield call(addPostAPI, action.data);
     yield put({
-      type: 'ADD_POST_SUCCESS',
+      type: ADD_POST_SUCCESS,
       // data: result.data
     });
   } catch (err) {
     yield put({
-      type: 'ADD_POST_FAILURE',
+      type: ADD_POST_FAILURE,
       data: err.response.data,
     });
   }
 }
 
+function addCommentAPI(data) { //얘는 *붙이면 error 발생
+  return axios.post(`/api/post/${data.postId}/comment`, data);
+}
+
+function* addComment(action) {
+  try {
+    yield delay(1000);
+    // const result = yield call(addCommentAPI, action.data);
+    yield put({
+      type: ADD_COMMENT_SUCCESS,
+      // data: result.data
+    });
+  } catch (err) {
+    yield put({
+      type: ADD_COMMENT_FAILURE,
+      data: err.response.data,
+    });
+  }
+}
 
 function* watchAddPost() {
-  yield takeLatest('ADD_POST_REQUEST', addPost);
+  yield takeLatest(ADD_POST_REQUEST, addPost);
+}
+
+function* watchCommentPost() {
+  yield takeLatest(ADD_COMMENT_REQUEST, addComment);
 }
 
 export default function* postSaga(){
   yield all([
     fork(watchAddPost),
+    fork(watchCommentPost),
   ]);
 }
--- Front/sagas/user.js
+++ Front/sagas/user.js
@@ -1,5 +1,8 @@
 import {all, delay, fork, put, takeLatest} from "redux-saga/effects";
 import axios from "axios";
+import {LOG_IN_FAILURE, LOG_IN_REQUEST, LOG_IN_SUCCESS, LOG_OUT_FAILURE, LOG_OUT_REQUEST, LOG_OUT_SUCCESS, SIGN_UP_SUCCESS, SIGN_UP_REQUEST, SIGN_UP_FAILURE
+} from "../reducers/user";
+import signup from "../pages/signup";
 
 //all, fork 등은 saga effect
 
@@ -12,14 +15,14 @@ function* logIn(action) {
     yield delay(1000); //서버가 없어서 임시용
     // const result = yield call(logInAPI, action.data); ////call을 쓰는 이유? 테스트 하기가 좋아서
     yield put({
-      type: 'LOG_IN_SUCCESS',
+      type: LOG_IN_SUCCESS,
       data: action.data,
       // data: result.data
     });
   } catch (err) {
     yield put({
-      type: 'LOG_IN_FAILURE',
-      data: err.response.data,
+      type: LOG_IN_FAILURE,
+      error: err.response.data,
     });
   }
 }
@@ -33,13 +36,33 @@ function* logOut() {
     yield delay(1000);
     // const result = yield call(logOutAPI);
     yield put({
-      type: 'LOG_OUT_SUCCESS',
+      type: LOG_OUT_SUCCESS,
       // data: result.data
     });
   } catch (err) {
     yield put({
-      type: 'LOG_OUT_FAILURE',
-      data: err.response.data,
+      type: LOG_OUT_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function signUpAPI() { //얘는 *붙이면 error 발생
+  return axios.post('/api/signUp')
+}
+
+function* signUp() {
+  try {
+    yield delay(1000);
+    // const result = yield call(signUpAPI);
+    yield put({
+      type: SIGN_UP_SUCCESS,
+      // data: result.data
+    });
+  } catch (err) {
+    yield put({
+      type: SIGN_UP_FAILURE,
+      error: err.response.data,
     });
   }
 }
@@ -49,16 +72,21 @@ function* logOut() {
 //takeLatest : 실수로 누른건 무시되고 마지막만 실행
 //takeReading : 처음꺼만 실행
 function* watchLogIn() {
-  yield takeLatest('LOG_IN_REQUEST', logIn); //yield take 일회용 => takeEvery 사용 (while true 사용한것과 동일)
+  yield takeLatest(LOG_IN_REQUEST, logIn); //yield take 일회용 => takeEvery 사용 (while true 사용한것과 동일)
 }
 
 function* watchLogOut() {
-  yield takeLatest('LOG_OUT_REQUEST', logOut);
+  yield takeLatest(LOG_OUT_REQUEST, logOut);
+}
+
+function* watchSignUp() {
+  yield takeLatest(SIGN_UP_REQUEST, signUp);
 }
 
 export default function* userSaga() {
   yield all([
     fork(watchLogIn),
     fork(watchLogOut),
+    fork(watchSignUp),
   ]);
 }
