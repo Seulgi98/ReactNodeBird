a722b789ca358195c786c5e6e1fef5e751b62904
Seulgi <hanny1226@naver.com>
2022-04-06 10:16:23 +0900

    Saga effects(take, takeEvery, takeLatest, takeReading, throttle)

--- Front/sagas/index.js
+++ Front/sagas/index.js
@@ -1,19 +1,21 @@
-import {all, fork, take, call, put} from "redux-saga/effects";
+import {all, fork, takeEvery, call, put, takeLatest, delay} from "redux-saga/effects";
 import axios from 'axios';
+
 //all, fork 등은 saga effect
 
-function logInAPI(data){ //얘는 *붙이면 error 발생
+function logInAPI(data) { //얘는 *붙이면 error 발생
   return axios.post('/api/login', data)
 }
 
-function* logIn(action){
+function* logIn(action) {
   try {
-    const result = yield call(logInAPI, action.data); //call을 쓰는 이유? 테스트 하기가 좋아
+    yield delay(1000); //서버가 없어서 임시용
+    // const result = yield call(logInAPI, action.data); ////call을 쓰는 이유? 테스트 하기가 좋아서
     yield put({
       type: 'LOG_IN_SUCCESS',
-      data: result.data
+      // data: result.data
     });
-  } catch (err){
+  } catch (err) {
     yield put({
       type: 'LOG_IN_FAILURE',
       data: err.response.data,
@@ -21,18 +23,19 @@ function* logIn(action){
   }
 }
 
-function logOutAPI(){ //얘는 *붙이면 error 발생
+function logOutAPI() { //얘는 *붙이면 error 발생
   return axios.post('/api/logout')
 }
 
-function* logOut(){
+function* logOut() {
   try {
-    const result = yield call(logInAPI);
+    yield delay(1000);
+    // const result = yield call(logOutAPI);
     yield put({
       type: 'LOG_OUT_SUCCESS',
-      data: result.data
+      // data: result.data
     });
-  } catch (err){
+  } catch (err) {
     yield put({
       type: 'LOG_OUT_FAILURE',
       data: err.response.data,
@@ -40,39 +43,43 @@ function* logOut(){
   }
 }
 
-function addPostAPI(data){ //얘는 *붙이면 error 발생
+function addPostAPI(data) { //얘는 *붙이면 error 발생
   return axios.post('/api/post', data);
 }
 
-function* addPost(action){
+function* addPost(action) {
   try {
-    const result = yield call(addPostAPI, action.data);
+    yield delay(1000);
+    // const result = yield call(addPostAPI, action.data);
     yield put({
       type: 'ADD_POST_SUCCESS',
-      data: result.data
+      // data: result.data
     });
-  } catch (err){
+  } catch (err) {
     yield put({
       type: 'ADD_POST_FAILURE',
       data: err.response.data,
     });
   }
 }
-
-function* watchLogIn(){
-  yield take('LOG_IN_REQUEST', logIn);
+//take : 일회성
+//takeEvery : 다회성
+//takeLatest : 실수로 누른건 무시되고 마지막만 실행
+//takeReading : 처음꺼만 실행
+function* watchLogIn() {
+  yield takeLatest('LOG_IN_REQUEST', logIn); //yield take 일회용 => takeEvery 사용 (while true 사용한것과 동일)
 }
 
-function* watchLogOut(){
-  yield take('LOG_OUT_REQUEST', logOut);
+function* watchLogOut() {
+  yield takeLatest('LOG_OUT_REQUEST', logOut);
 }
 
-function* watchAddPost(){
-  yield take('ADD_POST_REQUEST', addPost);
+function* watchAddPost() {
+  yield takeLatest('ADD_POST_REQUEST', addPost);
 }
 
 //call, fork 둘다 함수 실행, 다른점 :
-export default function* rootSaga(){ //generator
+export default function* rootSaga() { //generator
   yield all([ //배열안에 들어간 모든것을 실행
     fork(watchLogIn), //fork : 함수를 실행
     fork(watchLogOut),
