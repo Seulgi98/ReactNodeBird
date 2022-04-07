de0acd221dd9b5422ac7552f1d2439a580c28996
Seulgi <hanny1226@naver.com>
2022-04-07 13:56:12 +0900

    Delete button error

--- Front/components/PostCard.js
+++ Front/components/PostCard.js
@@ -21,8 +21,11 @@ const PostCard = ({ post }) => {
   const { removePostLoading } = useSelector((state) => state.post);
   const [commentFormOpened, setCommentFormOpened] = useState(false);
   const [liked, setLiked] = useState(false);
-  const { me } = useSelector((state) => state.user);
-  const id = me && me.id;
+  const id = useSelector((state) => state.user.me && state.user.me.id);
+  // const id = useSelector((state) => state.user.me?.id);
+
+  // const { me } = useSelector((state) => state.user);
+  // const id = me && me.id;
 
   const onToggleComment = useCallback(() => {
     setCommentFormOpened((prev) => !prev); //false는 true로, true는 false로 바뀜
@@ -33,19 +36,83 @@ const PostCard = ({ post }) => {
   }, []);
 
   const onRemovePost = useCallback(() => {
+    if (!id) {
+      return alert('로그인이 필요합니다.');
+    }
     dispatch({
       type: REMOVE_POST_REQUEST,
       data: post.id, //사용자 id는 user me.id에 있음
     });
-  }, []);
+  }, [id]);
 
+  // return (
+  //   <CardWrapper key={post.id}>
+  //     <Card
+  //       cover={post.Images[0] && <PostImages images={post.Images} />}  //이미지가 1개 이상 있을 때
+  //       actions={[
+  //         <RetweetOutlined key="retweet" />,
+  //         liked //좋아요를 눌렀을때 꽉 찬 하트
+  //           ? <HeartTwoTone twoToneColor="#eb2f96" key="heart" onClick={onToggleLike} />
+  //           : <HeartOutlined key="heart" onClick={onToggleLike} />,
+  //         <MessageOutlined key="message" onClick={onToggleComment} />,
+  //         <Popover
+  //           key="more"
+  //           content={(
+  //             <Button.Group>
+  //               {id && post.UserId === id
+  //                 ? ( //게시글 id와 로그인 id가 같다면
+  //                   <>
+  //                     <Button>수정</Button>
+  //                     <Button type="danger" loading={removePostLoading} onClick={onRemovePost}>삭제</Button>
+  //                   </>
+  //                 )
+  //                 : <Button>신고</Button>}
+  //             </Button.Group>
+  //           )}
+  //         >
+  //           <EllipsisOutlined />
+  //         </Popover>,
+  //       ]}
+  //       extra={<FollowButton post={post} />}
+  //     >
+  //       <Card.Meta
+  //         avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+  //         title={post.User.nickname}
+  //         description={<PostCardContent postData={post.content} />}
+  //       />
+  //     </Card>
+  //     {commentFormOpened && (
+  //       <>
+  //         <CommentForm post={post} />
+  //         <List
+  //           header={`${post.Comments ? post.Comments.length : 0} 댓글`}
+  //           itemLayout="horizontal"
+  //           dataSource={post.Comments || []}
+  //           renderItem={(item) => (
+  //             <li>
+  //               <Comment
+  //                 author={item.User.nickname}
+  //                 avatar={(
+  //                   <Link href={{ pathname: '/user', query: { id: item.User.id } }} as={`/user/${item.User.id}`}>
+  //                     <a><Avatar>{item.User.nickname[0]}</Avatar></a>
+  //                   </Link>
+  //                 )}
+  //                 content={item.content}
+  //               />
+  //             </li>
+  //           )}
+  //         />
+  //       </>
+  //     )}
+  //   </CardWrapper>
+  // );
   return (
     <CardWrapper key={post.id}>
       <Card
-        cover={post.Images[0] && <PostImages images={post.Images} />}  //이미지가 1개 이상 있을 때
+        cover={post.Images[0] && <PostImages images={post.Images} />}
         actions={[
           <RetweetOutlined key="retweet" />,
-          liked //좋아요를 눌렀을때 꽉 찬 하트
+          liked
             ? <HeartTwoTone twoToneColor="#eb2f96" key="heart" onClick={onToggleLike} />
             : <HeartOutlined key="heart" onClick={onToggleLike} />,
           <MessageOutlined key="message" onClick={onToggleComment} />,
@@ -53,11 +120,11 @@ const PostCard = ({ post }) => {
             key="ellipsis"
             content={(
               <Button.Group>
-                {id && post.UserId === id
-                  ? ( //게시글 id와 로그인 id가 같다면
+                {id && post.User.id === id
+                  ? (
                     <>
                       <Button>수정</Button>
-                      <Button type="danger" loading={removePostLoading} onClick={onRemovePost}>삭제</Button>
+                      <Button type="danger">삭제</Button>
                     </>
                   )
                   : <Button>신고</Button>}
@@ -79,9 +146,9 @@ const PostCard = ({ post }) => {
         <>
           <CommentForm post={post} />
           <List
-            header={`${post.Comments ? post.Comments.length : 0} 댓글`}
+            header={`${post.Comments.length} 댓글`}
             itemLayout="horizontal"
-            dataSource={post.Comments || []}
+            dataSource={post.Comments}
             renderItem={(item) => (
               <li>
                 <Comment
